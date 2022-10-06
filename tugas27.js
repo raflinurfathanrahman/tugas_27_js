f2b3cc6e4a7c064e9fa4763d9653c0ee9405c1d3
raflirahman <raflirahman375@gmail.com>
2022-10-06 13:33:38 +0700

    add tugas 27

---
+++ tugas27.js
@@ -0,0 +1,13 @@
+var a = "No ";
+var b = 'rumah saya ';
+var c = 30;
+var d = "A"
+var hasil = String(c)
+var result = a + b + c + d;
+console.log(result);
+console.log(result);
+var x = "Jalan pangeran Tirtayasa No";
+var y = 70;
+var z = String(y)
+var jumlah = x + y;
+console.log(jumlah);
